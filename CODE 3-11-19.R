bc17d7ab0098e5f2b713349ad4f8f6552ebf6b70
Steve <levkoff@gmail.com>
2019-03-13 14:41:33 -0700

    added CODE 3-11-19.R

--- CODE 3-11-19.R
+++ CODE 3-11-19.R
@@ -3,25 +3,33 @@
 
 library(datasets)
 View(mtcars)
-
+dim(mtcars)
 pairs(mtcars[1:4])
 
-MODEL1<-lm(mpg~hp, mtcars)
+MODEL1<-lm(mpg~0+hp, mtcars)
 summary(MODEL1)  ##summarize model results
 str(MODEL1)  ##summarizes structure of object
+str(mtcars)
+hist(MODEL1$residuals)
+summary(MODEL1$residuals)
+class(mtcars)
+x<-1
+y<-"Steve"
+class(y)
+class(x)
 class(MODEL1) ##returns class of object
+mean(mtcars$mpg)
 
 ##Verifying the pseudoinverse method
 dim(mtcars) ##checks dimension of mtcars dataset
 
 ##adds column of ones to the input data and changes the class from data.frame to matrix
 X<-cbind(as.matrix(rep(1,times=dim(mtcars)[1])), as.matrix(mtcars)[,2:11])  ##creates the X matrix and adds a column of 1's for the intercept term
-
+View(X)  
+dim(X)
 ##changes the class of the output variable to a matrix class
 y<-as.matrix(mtcars[,1])
-
-View(X)  
-
+View(y)
 ##NOTE THE solve() function is how R computes an inverse matrix
 ##the t() function is used to take the transpose of a matrix
 ##the expression %*% is uses to multiply two matrix objects together
@@ -30,19 +38,22 @@ dim(PseudoInverse)  ##check dimensions
 
 Beta<-PseudoInverse%*%y  ##recovers the optimal parameter vector
 Beta  ##reports the Beta vector
-MODEL1<-lm(mpg ~., mtcars)  ##regresses mpg on ALL variables
-MODEL1$coefficients  ##looks at the coefficient vector (BETA) to compare to the pseudoinverse method
+MODEL_A<-lm(mpg ~., mtcars)  ##regresses mpg on ALL variables
+MODEL_A$coefficients  ##looks at the coefficient vector (BETA) to compare to the pseudoinverse method
 
 MODEL2<-lm(mpg~hp, mtcars)
 summary(MODEL2)
 
+###incorporate nonlinear transformations
 MODEL3<-lm(mpg~log(hp), mtcars)
 summary(MODEL3)
+
+?mtcars
 
 MODEL4<-lm(log(mpg)~log(hp), mtcars)
 summary(MODEL4)
 
-MODEL5<-lm(mpg~I(hp)+I(hp^2), mtcars)
+MODEL5<-lm(mpg~I(hp)+I(hp^2), mtcars)  ##polynomial regression of order 2
 summary(MODEL5)
 
 MODEL6<-lm(mpg~poly(hp, 2, raw=TRUE), mtcars) ##alternatively
